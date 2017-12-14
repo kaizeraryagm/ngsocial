9379268c42e003e6fbb9daad353640c443149998
Kaizer Arya <kaizerarya@gmail.com>
2017-12-13 19:11:55 -0600

    angular

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-angular.module('ngSocial.facebook', ['ngRoute'])
+angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('facebook', {
@@ -9,6 +9,21 @@ angular.module('ngSocial.facebook', ['ngRoute'])
     controller: 'FacebookCtrl'
   });
 }])
+
+.config( function ( $facebookProvider ) {
+  $facebookProvider.setAppId('1961845387411204');
+  $facebookProvider.setPermissions("email", "public_profile", "user_posts", "publish_actions", "user_photos");
+})
+
+.run( function ($rootScope) {
+    (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+})
 
 .controller('FacebookCtrl', [function() {
 
