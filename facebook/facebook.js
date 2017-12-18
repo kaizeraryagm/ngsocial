902e37ea871fffc67d7939b12dc42d2ca6329b49
Kaizer Arya <kaizerarya@gmail.com>
2017-12-18 17:20:27 -0600

    profile pic

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -43,7 +43,7 @@ angular.module('ngSocial.facebook', ['ngRoute', 'ngFacebook'])
         }
 
     function refresh() {
-        $facebook.api("/me",{ fields: 'last_name,first_name,email,gender,locale' }).then(function(response) {
+        $facebook.api("/me",{ fields: 'last_name,first_name,email,gender,locale,link' }).then(function(response) {
             $scope.welcomeMsg = "Welcome " + response.first_name;
             $scope.isLoggedIn = true;
             $scope.userInfo = response;
